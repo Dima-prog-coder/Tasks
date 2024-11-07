a93b3de333a593734c3e284836faa04dfe664a85
unknown <platovdima6@gmail.com>
2024-11-07 08:14:10 +0300

    Изменен task7. Теперь в нем все работает через стандартные массивы(а не через связку HashSet + List). Также добален package utils для работы с коллекциями.

--- ru/vsu/cs/dplatov/vvp/task7/Main.java
+++ ru/vsu/cs/dplatov/vvp/task7/Main.java
@@ -1,101 +1,84 @@
 package ru.vsu.cs.dplatov.vvp.task7;
 
-import java.lang.reflect.Array;
+import ru.vsu.cs.dplatov.vvp.task7.util.ArrayUtils;
+
 import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
         test();
-        System.out.print(findLongestSubsequenceWithDifferentElements(readListFromConsole("Somelist")));
+        System.out.print(findLongestSubsequenceWithDifferentElements(ArrayUtils.readIntArrayFromConsole()));
     }
 
-    public static List<Integer> findLongestSubsequenceWithDifferentElements(List<Integer> lst) {
+    public static int[] findLongestSubsequenceWithDifferentElements(int[] lst) {
         int firstSubsequenceEl = 0;
         int maxLength = 0;
-        int r = 0;
+        int r = 1;
         int l = 0;
-        for (; r <= lst.size(); r++) {
-            List<Integer> currentSlice = listSlice(lst, l, r);
-            HashSet<Integer> set = new HashSet<>(currentSlice);
-            while (set.size() != currentSlice.size()) {
+        for (; r <= lst.length; r++) {
+            int [] currentSlice = listSlice(lst, l, r);
+            while (isRepeatingElExists(currentSlice)) {
                 l += 1;
-                currentSlice = listSlice(lst, l, r);
-                set = new HashSet<>(currentSlice);
+                currentSlice = listSlice(lst, l ,r);
             }
-            if (r - l >= maxLength) {
+             if (r - l >= maxLength) {
                 firstSubsequenceEl = l;
                 maxLength = r - l;
             }
         }
-        return new ArrayList<Integer>(Arrays.asList(firstSubsequenceEl, maxLength));
+        return new int[]{firstSubsequenceEl, maxLength};
     }
 
-    public static List<Integer> listSlice(List<Integer> lst, int l, int r) {
-        List<Integer> slice = new ArrayList<>();
-        for (int i = l; i < r; i++) {
-            slice.add(lst.get(i));
+    public static boolean isRepeatingElExists(int[] lstSlice){
+        int newEl = lstSlice[lstSlice.length-1];
+        for (int i = 0; i<lstSlice.length-1; i++){
+            int el = lstSlice[i];
+            if (el==newEl){
+                return true;
+            }
+        }
+    return false;
+    }
+
+    public static int[] listSlice(int[] lst, int l, int r) {
+        int[] slice = new int[r-l];
+        for (int i = 0; r-l>i; i++) {
+            slice[i] = lst[l+i];
         }
         return slice;
     }
 
-    public static List<Integer> toIntList(String s) {
-        Scanner scanner = new Scanner(s);
-        scanner.useLocale(Locale.ROOT);
-        scanner.useDelimiter("(\\s|[,;])+");
-        List<Integer> retLst = new ArrayList<>();
-        while (scanner.hasNext()) {
-            retLst.add(scanner.nextInt());
-        }
-        return retLst;
-    }
-
-    public static List<Integer> readListFromConsole(String listName) {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            try {
-                if (listName == null || listName.isEmpty()) {
-                    listName = " ";
-                }
-                System.out.printf("Введите список %s : ", listName);
-                String line = scanner.nextLine();
-                return toIntList(line);
-
-            } catch (Exception e) {
-                System.out.println("Ошибка ввода");
-            }
-        }
-    }
-
     public static void test(){
-        System.out.println("1,2,3,4,5,6,1,1,2,3,4,5,6,7,8,9");
-        System.out.println(findLongestSubsequenceWithDifferentElements(List.of(1,2,3,4,5,6,1,1,2,3,4,5,6,7,8,9)));
+        System.out.println("1,2,3,1,4");
+        System.out.println(Arrays.toString(findLongestSubsequenceWithDifferentElements(new int[]{1,2,3,1,4})));
 
         System.out.println("1,2,2,3,4,2,2,11");
-        System.out.println(findLongestSubsequenceWithDifferentElements(List.of(1,2,2,3,4,2,2,1)));
+        System.out.println(Arrays.toString(findLongestSubsequenceWithDifferentElements(new int[]{1, 2, 2, 3, 4, 2, 2, 1})));
 
         System.out.println("5,5,5,5,5,5,5");
-        System.out.println(findLongestSubsequenceWithDifferentElements(List.of(5,5,5,5,5,5,5)));
+        System.out.println(Arrays.toString(findLongestSubsequenceWithDifferentElements(new int[]{5, 5, 5, 5, 5, 5, 5})));
 
         System.out.println("1,2,3,4,5,6,7,8,9");
-        System.out.println(findLongestSubsequenceWithDifferentElements(List.of(1,2,3,4,5,6,7,8,9)));
+        System.out.println(Arrays.toString(findLongestSubsequenceWithDifferentElements(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9})));
 
         System.out.println("1,2,1,2,1,2,3,4,5");
-        System.out.println(findLongestSubsequenceWithDifferentElements(List.of(1,2,1,2,1,2,3,4,5)));
+        System.out.println(Arrays.toString(findLongestSubsequenceWithDifferentElements(new int[]{1, 2, 1, 2, 1, 2, 3, 4, 5})));
 
         System.out.println("1");
-        System.out.println(findLongestSubsequenceWithDifferentElements(List.of(1)));
+        System.out.println(Arrays.toString(findLongestSubsequenceWithDifferentElements(new int[]{1})));
 
         System.out.println("10, 20, 30, 40, 50, 20, 30, 40, 50, 60, 70");
-        System.out.println(findLongestSubsequenceWithDifferentElements(List.of(10, 20, 30, 40, 50, 20, 30, 40, 50, 60, 70)));
+        System.out.println(Arrays.toString(findLongestSubsequenceWithDifferentElements(new int[]{10, 20, 30, 40, 50, 20, 30, 40, 50, 60, 70})));
 
         System.out.println("1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7");
-        System.out.println(findLongestSubsequenceWithDifferentElements(List.of(1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7)));
+        System.out.println(Arrays.toString(findLongestSubsequenceWithDifferentElements(new int[]{1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7})));
 
         System.out.println("100, 101, 102, 103, 104, 105, 100, 106, 107, 108, 109");
-        System.out.println(findLongestSubsequenceWithDifferentElements(List.of(100, 101, 102, 103, 104, 105, 100, 106, 107, 108, 109)));
+        System.out.println(Arrays.toString(findLongestSubsequenceWithDifferentElements(new int[]{100, 101, 102, 103, 104, 105, 100, 106, 107, 108, 109})));
 
         System.out.println("5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 6, 7, 8, 9, 10");
-        System.out.println(findLongestSubsequenceWithDifferentElements(List.of(5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 6, 7, 8, 9, 10)));
+        System.out.println(Arrays.toString(findLongestSubsequenceWithDifferentElements(new int[]{5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 6, 7, 8, 9, 10})));
     }
 
 }
