e8f5b416183036903fa504a8b1e789b4340bed62
Dmitrii Platov <platovdima6@gmail.com>
2024-11-21 22:12:56 +0300

    Delete GraficalApps directory

--- GraficalApps/task9/ProgressionApp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.ru.vsu.cs.dplatov.vvp.task9.progressionapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.ru.vsu.cs.dplatov.vvp.task9.progressionapp to javafx.fxml;
-    exports com.ru.vsu.cs.dplatov.vvp.task9.progressionapp;
-}
